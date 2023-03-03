231b017cfbca5c9523705cf08f1d8899c46442af
Gina <9094115+ginazampino@users.noreply.github.com>
2023-03-03 18:58:16 -0500

    Added Electron

---
+++ src/electron/main.ts
@@ -0,0 +1,34 @@
+import { join } from 'path';
+import { app, BrowserWindow } from 'electron';
+
+const isDev = process.env.npm_lifecycle_event === 'app:dev' ? true : false;
+
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: join(__dirname, 'preload.js'),
+        },
+    });
+
+    mainWindow.loadURL(
+        isDev ? 'http://localhost:5173' : join(__dirname, '../../index.html')
+    );
+
+    if (isDev) {
+        mainWindow.webContents.openDevTools();
+    }
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit();
+});
